fdf63297b05d0d7d097102f1d4c1b95e60847bd3
Ezgi Kahraman <okyanusdolusukitap@gmail.com>
2019-01-22 17:28:18 +0300

    Ayarlar eklendi.

---
+++ inc/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// Veritabanı sunucu adı    
+$config['database']['host']  = "localhost";
+// Veritabanı adı
+$config['database']['name']  = "otel";
+// Veritabanı sunucu bağlantısı için gerekli olan kullanıcı adı
+$config['database']['username']  = "root";
+// Veritabanı sunucu bağlantısı için gerekli olan kullanıcı şifresi
+$config['database']['password']  = null;
+
+
+
+// SİTE GENEL AYARLAR
+$config['site']['theme'] = 'default';
+$config['site']['title'] = 'Otel rezervasyon sistemi';
+
+// ADMİN GENEL AYARLAR
+$config['admin']['theme'] = 'admin';
